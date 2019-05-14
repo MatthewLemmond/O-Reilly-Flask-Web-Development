6221bfc5fa86fde95200770b66ba0a1403d56c36
Matthew Lemmond <matthewjlemmond@gmail.com>
2019-05-14 03:07:08 -0400

    Add ability to change email

--- flasky.py
+++ flasky.py
@@ -16,5 +16,5 @@ def make_shell_context():
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('unittests')
     unittest.TextTestRunner(verbosity=2).run(tests)
